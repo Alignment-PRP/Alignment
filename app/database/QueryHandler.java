d7a89fa4782efd903372d5b0d9d99f83a244d714
Vestein Dahl <vesteind@stud.ntnu.no>
2017-02-15 14:44:01 +0100

    Partially completes unit testing for query-handling.

--- app/database/QueryHandler.java
+++ app/database/QueryHandler.java
@@ -15,47 +15,48 @@ import java.util.Map;
 //TODO Doc
 public class QueryHandler {
 
-  private final Database db;
+    private final Database db;
 
-  public QueryHandler(Database db) {
-    this.db = db;
-  }
-
-  //Example
-  public JsonNode getUserByName(String name) {
-    return executeQuery(Statement.GET_USER_BY_NAME, name);
-  }
-
-  //TODO Doc
-  public JsonNode executeQuery(Statement statement, Object... objects) {
-    try {
-      Connection c = db.getConnection();
-      JsonNode json = resultSetToJson(statement.prepareAndExecute(c, objects));
-      c.close();
-      return json;
-    } catch (SQLException e) {
-      return Json.toJson("Something went wrong");
+    public QueryHandler(Database db) {
+        this.db = db;
     }
-  }
 
-  private JsonNode resultSetToJson(ResultSet rs) {
-    List<Map<String, String>> list = new ArrayList<>();
-    try {
-      while (rs.next()) {
-        Map<String, String> map = new HashMap<>();
-        for (int i = 1; i<= rs.getMetaData().getColumnCount(); i++) {
-          String k = rs.getMetaData().getColumnName(i);
-          String v = rs.getString(k);
-          map.put(k, v);
+    //Example
+    @Deprecated
+    public JsonNode getUserByName(String name) {
+        return executeQuery(Statement.GET_USER_BY_NAME, name);
+    }
+
+    //TODO Doc
+    public JsonNode executeQuery(Statement statement, Object... objects) {
+        try {
+            Connection c = db.getConnection();
+            JsonNode json = resultSetToJson(statement.prepareAndExecute(c, objects));
+            c.close();
+            return json;
+        } catch (SQLException e) {
+            return Json.toJson("Something went wrong");
         }
-        list.add(map);
-      }
-    } catch (SQLException e) {
-      //TODO Some sort of errorhandling.
-      e.printStackTrace();
-      return Json.toJson(e.toString());
     }
-    return Json.toJson(list);
-  }
+
+    private JsonNode resultSetToJson(ResultSet rs) {
+        List<Map<String, String>> list = new ArrayList<>();
+        try {
+            while (rs.next()) {
+                Map<String, String> map = new HashMap<>();
+                for (int i = 1; i<= rs.getMetaData().getColumnCount(); i++) {
+                    String k = rs.getMetaData().getColumnName(i);
+                    String v = rs.getString(k);
+                    map.put(k, v);
+                }
+                list.add(map);
+            }
+        } catch (SQLException e) {
+            //TODO Some sort of errorhandling.
+            e.printStackTrace();
+            return Json.toJson(e.toString());
+        }
+        return Json.toJson(list);
+    }
 
 }
