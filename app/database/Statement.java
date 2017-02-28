823f05b627a7104942aab79839014b1fcdfc89f8
andrfo <xerion51@gmail.com>
2017-02-28 13:04:23 +0100

    can now get projects related to user along with project owner and project manager

--- app/database/Statement.java
+++ app/database/Statement.java
@@ -9,13 +9,36 @@ import java.sql.SQLException;
 public enum Statement {
   GET_PROJECT_BY_ID("SELECT *  FROM project WHERE projectid=?"),
   GET_PROJECTS_RELATED_TO_USER(
+
+          //TODO: Should probably make the nested queries into views.
           "SELECT * " +
-                  "FROM project " +
-                  "JOIN partof " +
-                  "ON project.projectid = partof.projectid " +
-                  "JOIN user " +
-                  "ON partof.userid = user.userid " +
-                  "WHERE user.userid= ?"),
+                  "FROM " +
+                        //Gets all user related projects
+                        "(SELECT p.projectid AS 'pid', p.name AS 'p_name', p.description AS 'p_desc' " +
+                            "FROM project p " +
+                            "JOIN partof " +
+                            "ON p.projectid = partof.projectid " +
+                            "JOIN user u " +
+                            "ON partof.userid = u.userid " +
+                            "WHERE u.userid= ?) AS part " +
+
+                  "INNER JOIN " +
+                        //Gets all the users that are projects owners of at least one project
+                        "(SELECT u.userid AS 'po_userid', u.username AS 'po_username', po.projectid AS 'po_pid' " +
+                          "FROM user u " +
+                          "JOIN projectowner po " +
+                          "ON u.userid = po.userid) AS pos " +
+                  "ON pos.po_pid = part.pid " +
+
+                  "INNER JOIN " +
+                        //Gets all the users that are project managers for at least one project
+                        "(SELECT u.userid AS 'pm_userid', u.username AS 'pm_username', pm.projectid AS 'pm_pid' " +
+                          "FROM user u " +
+                          "JOIN projectmanager pm " +
+                          "ON u.userid = pm.userid) AS pms " +
+                  "ON pms.pm_pid = part.pid"
+
+            ),
   GET_PUBLIC_PROJECTS(
           "SELECT * " +
                   "FROM project " +
