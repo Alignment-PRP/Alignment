a927d6a69a767396e535897358a8ddc745255b6b
Glenn Aarøen <glaar90@gmail.com>
2017-03-02 22:10:33 +0100

    add Header div and change site font

--- app/assets/javascripts/Root.jsx
+++ app/assets/javascripts/Root.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Sidebar from './Layout/Sidebar.jsx';
+import Header from './Layout/Header.jsx';
 
 export default class Root extends React.Component {
 
     render() {
         return (
             <div>
+                <Header />
                 <Sidebar/>
             </div>
         );
