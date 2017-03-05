0ab8950303826a79d6ef6a7cd37272bd8e4ac197
Glenn Aarøen <glaar90@gmail.com>
2017-03-06 00:37:08 +0100

    done with single project react components

--- app/assets/javascripts/projects/ProjectList.jsx
+++ app/assets/javascripts/projects/ProjectList.jsx
@@ -10,8 +10,6 @@ export default class ProjectList extends React.Component {
     render() {
 
         const url = "project/" + this.props.index;
-        console.log(url);
-
 
         return (
             <div>
