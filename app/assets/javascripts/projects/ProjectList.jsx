fe3bf36f0d5508c890a4593c02fa35e9469211a1
Glenn Aarøen <glaar90@gmail.com>
2017-03-05 22:35:42 +0100

    add single-page-project-view

--- app/assets/javascripts/projects/ProjectList.jsx
+++ app/assets/javascripts/projects/ProjectList.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
+import {Link} from 'react-router';
 
-export default class Projects extends React.Component {
+export default class ProjectList extends React.Component {
 
     constructor(props) {
         super(props);
     }
 
     render() {
+
+        const url = "project/" + this.props.index;
+        console.log(url);
+
+
         return (
             <div>
                 <h1>{this.props.name}</h1>
                 <p>{this.props.descripton}</p>
                 <p>Prosjekt Eier: {this.props.owner}</p>
                 <p>Prosjekt Sjef: {this.props.manager}</p>
+                <p>Index: {this.props.index}</p>
+                <Link to={url}>{this.props.name}</Link>
             </div>
         );
     }
