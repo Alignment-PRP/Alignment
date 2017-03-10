93267d084053bf72f9a4e7fb6638813e169762a7
Glenn Aarøen <glaar90@gmail.com>
2017-03-10 10:56:28 +0100

    fix SideMeny.jsx merge conflict

--- app/assets/javascripts/sidemeny/SideMeny.jsx
+++ app/assets/javascripts/sidemeny/SideMeny.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import {Link} from 'react-router';
 
 export default class SideMeny extends React.Component {
 
+    constructor(props) {
+        super(props);
+
+    }
+
     render() {
+        var currentLocation = this.props.location.pathname;
+        console.log(currentLocation);
+
+
         return (
             <div id="sidemeny">
-                <h4>sidemeny</h4>
+                <h4> Meny </h4>
+                <ul>
+                    <Link to="#"> <p>Kategoriliste</p></Link>
+                </ul>
             </div>
         );
     }
-
 }
