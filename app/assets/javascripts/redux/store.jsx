82f895ec20066aa15af9a990d317043dfb0e94fd
Glenn Aarøen <glaar90@gmail.com>
2017-03-12 14:42:53 +0100

    clean up frontend code

--- app/assets/javascripts/redux/store.jsx
+++ app/assets/javascripts/redux/store.jsx
@@ -2,11 +2,9 @@ import {createStore, combineReducers, applyMiddleware} from "redux";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
 
-import math from "./reducers/mathReducer.jsx";
-import user from "./reducers/userReducer.jsx";
 import requirementReducer from "./reducers/requirementReducer.jsx";
 import projectReducer from "./reducers/projectReducer.jsx";
 
 export default createStore(
-    combineReducers({ math, user, requirementReducer, projectReducer }), {}, applyMiddleware(logger(), thunk)
+    combineReducers({ requirementReducer, projectReducer }), {}, applyMiddleware(logger(), thunk)
 );
--- app/assets/javascripts/utility/Logout.jsx
+++ app/assets/javascripts/utility/Logout.jsx
@@ -2,17 +2,13 @@ import React from 'react';
 
 export default class Logout extends React.Component {
 
-    constructor(props) {
-        super(props);
-    }
-
     componentDidMount() {
         window.location = "http://localhost:9000/logout";
     }
 
     render() {
         return (
-            <p></p>
+            <p>Logger ut...</p>
         );
     }
 }
