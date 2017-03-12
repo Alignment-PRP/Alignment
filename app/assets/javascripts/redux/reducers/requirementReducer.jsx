4bbd63e1a23f7a69a32ace90891997f0f6895785
Glenn Aarøen <glaar90@gmail.com>
2017-03-12 01:55:01 +0100

    add redux-thunk to fix asyncronus actions

--- app/assets/javascripts/redux/reducers/requirementReducer.jsx
+++ app/assets/javascripts/redux/reducers/requirementReducer.jsx
@@ -1,16 +1,27 @@
-const requirementReducer = (state = {
-    requirements: []
-}, action) => {
+import axios from 'axios';
+
+const getInitialState = () => {
+    const data = [];
+
+    axios.get('http://localhost:9000/requirements/all ')
+        .then( response => {
+            response.data.map((object) => {
+                data.push(object)
+            });
+        });
+
+    return {
+        requirements: data
+    }
+
+};
+
+
+const requirementReducer = (state = getInitialState(), action) => {
     switch (action.type) {
         case "GET_ALL_REQUIREMENTS":
             state = {
                 requirements: action.payload
-            };
-            break;
-        case "SUBTRACT":
-            state = {
-                result: state.result - action.payload,
-                lastValues: [...state.lastValues, action.payload]
             };
             break;
     }
