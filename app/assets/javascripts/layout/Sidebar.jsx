fe23f5209d370a9e0c3bb8d3e2fc15817c8272bc
Glenn Aarøen <glaar90@gmail.com>
2017-03-14 23:13:47 +0100

    update-sidemenu

--- app/assets/javascripts/layout/Sidebar.jsx
+++ app/assets/javascripts/layout/Sidebar.jsx
@@ -69,7 +69,6 @@ class Sidebar extends React.Component {
                     <Menu autoWidth={false} width={290}>
                         <MenuItem primaryText="Hjem" onClick={() => this.props.changeSideMenuMode("MENU")} containerElement={<Link to="/" />} rightIcon={<FontIcon style={this.style.rightIcon} className="material-icons">home</FontIcon>}/>
                         <MenuItem primaryText="Prosjekter"containerElement={<Link to="projects" />} rightIcon={<FontIcon style={this.style.rightIcon} className="material-icons">assignment</FontIcon>} />
-                        <MenuItem primaryText="Nytt Prosjekt"containerElement={<Link to="newproject" />} rightIcon={<FontIcon style={this.style.rightIcon} className="material-icons">build</FontIcon>} />
                         <MenuItem primaryText="Krav"containerElement={<Link to="allrequirements" />} rightIcon={<FontIcon style={this.style.rightIcon} className="material-icons">speaker_notes</FontIcon>}/>
                         <MenuItem primaryText="Logg ut"containerElement={<Link to="logout" />} rightIcon={<FontIcon style={this.style.rightIcon} className="material-icons">directions_run</FontIcon>}/>
                     </Menu>
