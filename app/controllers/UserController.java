a483ea17fa6670df04f804b82bdfeb3152861963
Risvaag <andrris@stud.ntnu.no>
2017-03-14 22:36:04 +0100

    should fetch username and a string representing userClass for user

--- app/controllers/UserController.java
+++ app/controllers/UserController.java
@@ -2,13 +2,17 @@ package controllers;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import database.QueryHandler;
+import database.Statement;
 import models.User;
 import play.api.db.DB;
 import play.db.Database;
 import play.libs.Json;
 import play.mvc.Controller;
+import play.mvc.Result;
 
 import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by andrfo on 16.02.2017.
@@ -20,6 +24,16 @@ public class UserController extends Controller {
     @Inject
     public UserController(Database db) {
         this.qh = new QueryHandler(db);
+    }
+
+    public Result getUser(){
+        //TODO replace this with references to new DB's "userClass" table. (session username should be ok?)
+        List<String> liste = new ArrayList<>();
+        String userName = qh.executeQuery(Statement.GET_USER_NAME, session("connected")).get(0).get("userName").asText();
+        liste.add(userName);
+        liste.add("UserClassPlaceholder");
+        JsonNode result = Json.toJson(liste);
+        return ok(result);
     }
 
     public JsonNode makeJsonNode(String username){
