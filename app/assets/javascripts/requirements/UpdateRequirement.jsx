ed145025255a4b0864aea6d14a4d394e196245e8
Glenn Aarøen <glaar90@gmail.com>
2017-03-14 19:37:42 +0100

    done with edit requirement functionality

--- app/assets/javascripts/requirements/UpdateRequirement.jsx
+++ app/assets/javascripts/requirements/UpdateRequirement.jsx
@@ -9,46 +9,55 @@ class UpdateRequirement extends React.Component {
 		this.props.changeSideMenuMode("MENU");
 	}
 
+	renderCheckbox(){
+        if (this.props.requirement.ispublic == "1"){
+            return (<input type="checkbox" name="public" defaultChecked/>);
+        }else{
+            return (<input type="checkbox" name="public"/>);
+        }
+    }
+
 	render() {
+
 		return (
 			<form action="/update-requirement" method="post">
+                <h1>Oppdater Krav</h1>
 
-				<label><b>KravId(OBS dette bør gjøres gjennom å få id fra et eksisterende req (GET /get-requirement)</b></label><br/>
-				<input type="number" name="id" min="0" required/>
+				<label><b>KravId</b></label><br/>
+				<input type="number" defaultValue={this.props.requirement.requirementid} name="id" min="0" required/>
 
 				<br/>
 				<label><b>KravName</b></label><br/>
-				<input type="text" placeholder="Enter Projectname" name="name" required/>
+				<input type="text" defaultValue={this.props.requirement.name} name="name" required/>
 
                 <br/>
 				<label><b>description</b></label><br/>
-				<input type="text" placeholder="Enter description" name="description" required/>
+				<input type="text" defaultValue={this.props.requirement.description} name="description" required/>
 
                 <br/>
 
 				<label> <b>Source</b></label><br/>
-				<input type="text" placeholder="Source" name="source" required/>
+				<input type="text" defaultValue={this.props.requirement.source} name="source" required/>
 
                 <br/>
 				<label><b>Stimulus</b></label><br/>
-				<input type="text" placeholder="Strimulus" name="stimulus" required/>
+				<input type="text" defaultValue={this.props.requirement.stimulus} name="stimulus" required/>
 
                 <br/>
 				<label> <b>Artifact</b></label><br/>
-				<input type="text" placeholder="Artifact" name="artifact" required/>
+				<input type="text" defaultValue={this.props.requirement.artifact} name="artifact" required/>
 
                 <br/>
 				<label> <b>Response</b></label><br/>
-				<input type="text" placeholder="Response" name="response" required/>
+				<input type="text" defaultValue={this.props.requirement.response} name="response" required/>
 
                 <br/>
 				<label> <b>Environment</b></label><br/>
-				<input type="text" placeholder="Environment" name="environment" required/>
+				<input type="text" defaultValue={this.props.requirement.environment} name="environment" required/>
 
                 <br/>
 				<label><b>Public</b></label>
-
-                <input type="checkbox" name="public" />
+                {this.renderCheckbox()}
                 <br/>
                 <button type="submit">Oppdater</button>
 				</form>
@@ -58,7 +67,8 @@ class UpdateRequirement extends React.Component {
 
 const mapStateToProps = (state) => {
     return {
-        mode: state.sideMenuReducer.mode
+        mode: state.sideMenuReducer.mode,
+        requirement: state.requirementReducer.requirement
     };
 };
 
