f117f4687a3c1cef61530c525a71a01f3d5a3193
Vestein Dahl <vesteind@stud.ntnu.no>
2017-03-20 20:34:07 +0100

    Fixes tap-event.

--- app/assets/javascripts/main.jsx
+++ app/assets/javascripts/main.jsx
@@ -20,32 +20,37 @@ import NewRequirement from './requirements/NewRequirement.jsx';
 import {Router, Route, IndexRoute, browserHistory} from "react-router";
 import {Provider} from "react-redux";
 import store from './redux/store.jsx';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
+injectTapEventPlugin();
 
 //Defining url links
 class App extends React.Component {
 
     render() {
         return (
-            <Provider store={store}>
-                <Router history={browserHistory}>
-                    <Route path={"/"} component={Root}>
-                        <IndexRoute component={Home}/>
-                        <Route path={"login"} component={Home}/>
-                        <Route path={"projects"} component={Projects}/>
-                        <Route path={"/api/project/new"} component={Projects}/> {/* for rerouting purposes*/}
-                        <Route path={"project/:id"} component={Project}/>
-                        <Route path={"newproject"} component={NewProject}/>
-                        <Route path={"allrequirements"} component={AllRequirements}/>
-                        <Route path={"/api/requirement/add"} component={AllRequirements}/> {/* for rerouting purposes*/}
-                        <Route path={"/api/requirement/update"} component={AllRequirements}/> {/* for rerouting purposes*/}
-                        <Route path={"newrequirement"} component={NewRequirement}/>
-                        <Route path={"add-requirement"} component={AllRequirements}/>
-                        <Route path={"editrequirement"} component={UpdateRequirement}/>
-                        <Route path={"logout"} component={Logout}/>
-                    </Route>
-                </Router>
-            </Provider>
+            <MuiThemeProvider>
+                <Provider store={store}>
+                    <Router history={browserHistory}>
+                        <Route path={"/"} component={Root}>
+                            <IndexRoute component={Home}/>
+                            <Route path={"login"} component={Home}/>
+                            <Route path={"projects"} component={Projects}/>
+                            <Route path={"/api/project/new"} component={Projects}/> {/* for rerouting purposes*/}
+                            <Route path={"project/:id"} component={Project}/>
+                            <Route path={"newproject"} component={NewProject}/>
+                            <Route path={"allrequirements"} component={AllRequirements}/>
+                            <Route path={"/api/requirement/add"} component={AllRequirements}/> {/* for rerouting purposes*/}
+                            <Route path={"/api/requirement/update"} component={AllRequirements}/> {/* for rerouting purposes*/}
+                            <Route path={"newrequirement"} component={NewRequirement}/>
+                            <Route path={"add-requirement"} component={AllRequirements}/>
+                            <Route path={"editrequirement"} component={UpdateRequirement}/>
+                            <Route path={"logout"} component={Logout}/>
+                        </Route>
+                    </Router>
+                </Provider>
+            </MuiThemeProvider>
         );
     }
 
