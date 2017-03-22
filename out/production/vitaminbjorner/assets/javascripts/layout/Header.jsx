77c4d41591feb6cd486f0b3615cac8ee6e1c4af7
mokenikken <mbuashansen@hotmail.com>
2017-03-22 11:55:17 +0100

    Layout WIP

---
+++ out/production/vitaminbjorner/assets/javascripts/layout/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Header extends React.Component {
+
+    render() {
+        return (
+            <header id="header">
+                <h1>Quali-T</h1>
+            </header>
+        );
+    }
+
+}
