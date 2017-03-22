d5d49da7d75ea6e3138d7d9dbb6050f4bb0c4d5f
Vestein Dahl <vesteind@stud.ntnu.no>
2017-03-22 01:46:50 +0100

    Centralizes action types.

--- app/assets/javascripts/redux/reducers/projectReducer.jsx
+++ app/assets/javascripts/redux/reducers/projectReducer.jsx
@@ -1,24 +1,29 @@
+import {GET_ALL_PROJECTS,
+        GET_PROJECT_BY_ID,
+        GET_REQUIREMENTS_BY_PROJECT_ID
+} from './../types.jsx';
+
 const projectReducer = (state = {
     projects: [],
     project: [],
     projectRequirements: []
 }, action) => {
     switch (action.type) {
-        case "GET_ALL_PROJECTS":
+        case GET_ALL_PROJECTS:
             state = {
                 projects: action.payload,
                 project: state.project,
                 projectRequirements: state.projectRequirements
             };
             break;
-        case "GET_PROJECT_BY_ID":
+        case GET_PROJECT_BY_ID:
             state = {
                 projects: state.projects,
                 project: action.payload,
                 projectRequirements: state.projectRequirements
             };
             break;
-        case "GET_REQUIREMENTS_BY_PROJECT_ID":
+        case GET_REQUIREMENTS_BY_PROJECT_ID:
             state = {
                 projectRequirements: action.payload,
                 projects: state.projects,
