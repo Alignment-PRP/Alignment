970330a98ba6973987c83b04d79d29918e72593f
mokenikken <mbuashansen@hotmail.com>
2017-03-22 11:55:17 +0100

    Layout WIP

---
+++ out/production/vitaminbjorner/assets/javascripts/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class Home extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Velkommen</h1>
+            </div>
+        );
+    }
+}
