423d52681e0796c112bed45fa4a3eef1768c0cfa
andrfo <xerion51@gmail.com>
2017-03-24 14:05:21 +0100

    Fixed get requirements after adding new relational table in the database between requirement and subcategory

--- app/database/Statement.java
+++ app/database/Statement.java
@@ -38,8 +38,10 @@ public enum Statement {
             "FROM Requirements AS r " +
             "INNER JOIN RequirementMetaData AS rm " +
             "ON r.ID = rm.RID " +
+            "INNER JOIN HasSubCategory AS hsc " +
+            "ON r.ID = hsc.RID " +
             "INNER JOIN SubCategory AS sc " +
-            "ON rm.subCatID = sc.ID " +
+            "ON hsc.SID = sc.ID " +
             "INNER JOIN Category AS c " +
             "ON sc.catID = c.ID "),
 
