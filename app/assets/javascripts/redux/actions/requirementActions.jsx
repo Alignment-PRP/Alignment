1fd4553a40ca8d5917baaa633e160c017d7ac6f3
Glenn Aarøen <glaar90@gmail.com>
2017-03-24 05:08:11 +0100

    fix-wip, working on parsing

--- app/assets/javascripts/redux/actions/requirementActions.jsx
+++ app/assets/javascripts/redux/actions/requirementActions.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import * as URLS from './../../config.jsx';
 import {GET_ALL_REQUIREMENTS,
+        GET_ALL_CATEGORY_NAMES,
         UPDATE_FILTER_REQUIREMENT_LIST,
         UPDATE_FILTER,
         UPDATE_REQUIREMENT
@@ -17,14 +18,36 @@ export function getAllRequirements() {
                 });
                 dispatch(getAllRequirementsAsync(data))
             });
-
     }
-
 }
 
 function getAllRequirementsAsync(data) {
     return {
         type: GET_ALL_REQUIREMENTS,
+        payload: data
+    }
+}
+
+export function getAllCategoryNames() {
+    return dispatch => {
+        axios.get(URLS.CATEGORY_GET_ALL_NAMES)
+            .then( response => {
+                console.log(response);
+                const data = [];
+                response.data.map((object) => {
+                    data.push(object);
+                    return data
+                });
+                dispatch(getAllCategoryNamesAsync(data))
+            });
+
+    }
+
+}
+
+function getAllCategoryNamesAsync(data) {
+    return {
+        type: GET_ALL_CATEGORY_NAMES,
         payload: data
     }
 }
