5dc123fac5b9055e26716f166e867d983604868f
Glenn Aarøen <glaar90@gmail.com>
2017-03-24 11:08:38 +0100

    commented the dataflow of Projects.jsx

--- app/assets/javascripts/redux/store.jsx
+++ app/assets/javascripts/redux/store.jsx
@@ -7,6 +7,11 @@ import requirementReducer from "./reducers/requirementReducer.jsx";
 import projectReducer from "./reducers/projectReducer.jsx";
 import sideMenuReducer from "./reducers/sideMenuReducer.jsx";
 
+//This combine all the reducers into one.
+//The second argument of combineReducer can be used to to set initial global state.
+//The third argument applies middleware. This handles the data after it get dispatched from actions and before it gets to the reducers.
+// This application uses "logger" for debugging purposes in browser and "thunk" to handle the asynchronous calls in Actions.
+
 export default createStore(
     combineReducers({ userReducer, requirementReducer, projectReducer, sideMenuReducer }), {}, applyMiddleware(logger(), thunk)
 );
