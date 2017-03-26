d0d3be124c3eaf692b59cb9661aad5e08ab13174
Vestein Dahl <vesteindahl@hotmail.com>
2017-03-26 23:38:25 +0200

    Implements everything related to Admin view and user management. (#53)
    * WIP
    
    * Adds GET_USERS action.
    
    * Adds new action and reducer, userForm.
    
    * Implements functionality to fetch userdata from database.
    
    * Updates routes.
    
    * More work on admin-user related tasks.
    
    * Making progress.
    
    * WIP merge.
    
    * Fixes.
    
    * Userclass support.
    
    * Work
    
    * Stuff.
    
    * Submit works.
    
    * Almost done. Cleanup next.
    
    * Cleanup done.
    
    * More stuff.

--- app/assets/javascripts/redux/store.jsx
+++ app/assets/javascripts/redux/store.jsx
@@ -1,11 +1,24 @@
 import {createStore, combineReducers, applyMiddleware} from "redux";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
+import { reducer as formReducer } from 'redux-form'
 
 import userReducer from "./reducers/userReducer.jsx";
 import requirementReducer from "./reducers/requirementReducer.jsx";
 import projectReducer from "./reducers/projectReducer.jsx";
 import sideMenuReducer from "./reducers/sideMenuReducer.jsx";
+import userFormReducer from './reducers/userFormReducer.jsx';
+import adminTabReducer from './reducers/adminTabReducers.jsx';
+
+const reducers = {
+    userReducer,
+    requirementReducer,
+    projectReducer,
+    sideMenuReducer,
+    userFormReducer,
+    adminTabReducer,
+    form: formReducer,
+};
 
 //This combine all the reducers into one.
 //The second argument of combineReducer can be used to to set initial global state.
@@ -13,5 +26,5 @@ import sideMenuReducer from "./reducers/sideMenuReducer.jsx";
 // This application uses "logger" for debugging purposes in browser and "thunk" to handle the asynchronous calls in Actions.
 
 export default createStore(
-    combineReducers({ userReducer, requirementReducer, projectReducer, sideMenuReducer }), {}, applyMiddleware(logger(), thunk)
+    combineReducers(reducers), {}, applyMiddleware(logger(), thunk)
 );
