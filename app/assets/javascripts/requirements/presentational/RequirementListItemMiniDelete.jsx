6e47a0c0fc223bcf6e0063d2cecd28dc0f6cf3fe
Glenn Aarøen <glaar90@gmail.com>
2017-03-30 13:22:57 +0200

    add button on proReqList

---
+++ app/assets/javascripts/requirements/presentational/RequirementListItemMiniDelete.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class RequirementListItemMiniDelete extends React.Component {
+
+    render() {
+        const requirement = this.props.requirement;
+        return (
+            <tr>
+                <td>{requirement.name}</td>
+                <td>{requirement.description}</td>
+                <td><button onClick={() => this.props.onClickHandler(requirement)}>Legg til</button></td>
+            </tr>
+
+        );
+    }
+}
