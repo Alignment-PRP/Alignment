71c345e13f61207f874996f7b8619288e624c4eb
Vestein Dahl <vesteindahl@hotmail.com>
2017-04-02 23:45:41 +0200

    Changes ProjectTableRow button from RaisedButton to IconButton.

--- app/assets/javascripts/projects/presentational/ProjectTableRow.jsx
+++ app/assets/javascripts/projects/presentational/ProjectTableRow.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import {Link} from 'react-router';
 import {TableRow, TableRowColumn} from 'material-ui/Table';
 import RaisedButton from 'material-ui/RaisedButton';
+import IconButton from 'material-ui/IconButton';
+import FontIcon from 'material-ui/FontIcon';
 import {PROJECT_GET_BY_ID} from './../../config.jsx';
 
 /**
@@ -18,7 +20,7 @@ class ProjectTableRow extends React.Component {
                 <TableRowColumn>{project.name}</TableRowColumn>
                 <TableRowColumn>{project.creatorID}</TableRowColumn>
                 <TableRowColumn>{project.managerID}</TableRowColumn>
-                <TableRowColumn><Link to={PROJECT_GET_BY_ID + project.ID}><RaisedButton label="Endre"/></Link></TableRowColumn>
+                <TableRowColumn><Link to={PROJECT_GET_BY_ID + project.ID}><IconButton><FontIcon className="material-icons">edit</FontIcon></IconButton></Link></TableRowColumn>
             </TableRow>
         );
     }
