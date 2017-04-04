e6776650e5a12a9eab5c87b51ade2d8b386936f8
Glenn Aarøen <glaar90@gmail.com>
2017-04-04 15:49:19 +0200

    move render.jsx into admin folder, and add redux-form to add new requirement

--- app/assets/javascripts/requirements/NewRequirement.jsx
+++ app/assets/javascripts/requirements/NewRequirement.jsx
@@ -1,60 +1,21 @@
 import React from 'react';
-import * as URLS from './../config.jsx';
 import {connect} from "react-redux";
 import { changeSideMenuMode } from "../redux/actions/sideMenuActions.jsx";
+import { AddRequirement } from "../redux/actions/requirementActions.jsx";
 
-class NewRequirement extends React.Component {
+import NewRequirementForm from './form/NewRequirementForm.jsx';
 
-	componentDidMount(){
-		this.props.changeSideMenuMode("REQUIREMENTS_MENU");
-	}
+class UpdateRequirement extends React.Component {
 
-	render() {
+    componentDidMount(){
+        this.props.changeSideMenuMode("HIDE");
+    }
 
-		return (
-		    <div className="new-requirement">
-                <form action={URLS.REQUIREMENT_POST_ADD} method="post">
-                    <h2>Lag nytt krav</h2>
-
-                    <br/>
-
-                    <label><b>KravNavn</b></label><br/>
-                    <input type="text" placeholder="Kravname" name="name" required/>
-
-                    <br/>
-                    <label><b>Beskrivelse</b></label><br/>
-                    <input type="text" placeholder="Beskrivelse" name="description" required/>
-
-                    <br/>
-
-                    <label> <b>SubCategoryID</b></label><br/>
-                    <input type="text" placeholder="SubCategoryID" name="subCatID" required/>
-
-                    <br/>
-                    <label><b>reqResponsible</b></label><br/>
-                    <input type="text" placeholder="reqResponsible" name="reqResponsible" required/>
-
-                    <br/>
-                    <label> <b>comment</b></label><br/>
-                    <input type="text" placeholder="comment" name="comment" required/>
-
-                    <br/>
-                    <label> <b>reqCode</b></label><br/>
-                    <input type="text" placeholder="reqCode" name="reqCode" required/>
-
-                    <br/>
-                    <label> <b>reqNo</b></label><br/>
-                    <input type="text" placeholder="reqNo" name="reqNo" required/>
-
-                    <br/>
-                    <label><b>Public</b></label>
-                    <input type="checkbox" name="public"/>
-                    <br/>
-                    <button type="submit">Legg til</button>
-                </form>
-            </div>
-		);
-	}
+    render() {
+        return (
+            <NewRequirementForm onSubmit={this.props.addRequirement}/>
+        );
+    }
 }
 
 const mapStateToProps = (state) => {
@@ -67,8 +28,11 @@ const mapDispatchToProps = (dispatch) => {
     return {
         changeSideMenuMode: (mode) => {
             dispatch(changeSideMenuMode(mode))
+        },
+        addRequirement: (requirement) => {
+            dispatch(addRequirement(requirement))
         }
     };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(NewRequirement);
+export default connect(mapStateToProps, mapDispatchToProps)(UpdateRequirement);
