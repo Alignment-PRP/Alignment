e638291f522c1c13d512e540e43347c9ef3b2a56
Glenn Aarøen <glaar90@gmail.com>
2017-04-29 20:52:06 +0200

    make proReqDialog bigger

--- app/assets/javascripts/projects/dialog/ProjectReqUpdateDialog.jsx
+++ app/assets/javascripts/projects/dialog/ProjectReqUpdateDialog.jsx
@@ -7,7 +7,14 @@ class ProjectReqUpdateDialog extends React.Component {
     render() {
         const { handleSubmit, title, open, onRequestClose } = this.props;
         return (
-            <Dialog title={title} open={open} modal={false} onRequestClose={onRequestClose} >
+            <Dialog title={title}
+                    open={open}
+                    modal={false}
+                    onRequestClose={onRequestClose}
+                    autoDetectWindowHeight={false}
+                    autoScrollBodyContent={false}
+                    Style={{width: "100%"}}
+            >
                 <ProjectReqForm onSubmit={handleSubmit} disabled={false} handleClose={onRequestClose}/>
             </Dialog>
         );
