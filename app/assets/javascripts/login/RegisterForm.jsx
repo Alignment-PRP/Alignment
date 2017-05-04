b4d0483a3cfe8cd73ba58fc6b7de330c04aadc73
Vestein Dahl <vesteindahl@hotmail.com>
2017-05-04 23:11:43 +0200

    Changes accentColor and fixes form themes.

--- app/assets/javascripts/login/RegisterForm.jsx
+++ app/assets/javascripts/login/RegisterForm.jsx
@@ -1,10 +1,6 @@
-/**
- * Redux-form for user creation and updating.
- */
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 import {connect} from "react-redux";
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import {renderTextField, renderPassField, validateRegisterForm as validate} from './../core/render';
 import {Paper, RaisedButton} from "material-ui";
 
@@ -13,58 +9,56 @@ class RegisterForm extends React.Component {
     render() {
         const { handleSubmit, pristine, submitting, handleBackButton, register } = this.props;
         return (
-            <MuiThemeProvider>
-                <Paper id="loginForm">
-                    <h2>Ny Bruker</h2>
-                    {register.error ?
-                        <div id="loginFailed">Brukernavnet eksisterer</div>
-                        : null}
-                    <form onSubmit={handleSubmit}>
-                        <div id="loginFields">
-                            <Field
-                                name="USERNAME"
-                                label="Brukernavn"
-                                component={renderTextField}
-                                required
-                            />
-                            <Field
-                                name="email"
-                                label="Epost"
-                                component={renderTextField}
-                                required
-                            />
-                            <Field
-                                name="firstName"
-                                label="Fornavn"
-                                component={renderTextField}
-                                required
-                            />
-                            <Field
-                                name="lastName"
-                                label="Etternavn"
-                                component={renderTextField}
-                                required
-                            />
-                            <Field
-                                name="pass"
-                                label="Passord"
-                                component={renderPassField}
-                                required
-                            />
-                            <Field
-                                name="pass_rep"
-                                label="Gjenta Passord"
-                                component={renderPassField}
-                                required
-                            />
-                        </div>
-                        <div id="loginButtons">
-                            <RaisedButton className="form-button" primary={true} type="submit" label="Lagre" disabled={pristine || submitting}/>
-                            <RaisedButton className="form-button" secondary={true} label="Tilbake" onTouchTap={handleBackButton}/>
-                        </div>
-                    </form>
-                </Paper>
-            </MuiThemeProvider>
+            <Paper id="loginForm">
+                <h2>Ny Bruker</h2>
+                {register.error ?
+                    <div id="loginFailed">Brukernavnet eksisterer</div>
+                    : null}
+                <form onSubmit={handleSubmit}>
+                    <div id="loginFields">
+                        <Field
+                            name="USERNAME"
+                            label="Brukernavn"
+                            component={renderTextField}
+                            required
+                        />
+                        <Field
+                            name="email"
+                            label="Epost"
+                            component={renderTextField}
+                            required
+                        />
+                        <Field
+                            name="firstName"
+                            label="Fornavn"
+                            component={renderTextField}
+                            required
+                        />
+                        <Field
+                            name="lastName"
+                            label="Etternavn"
+                            component={renderTextField}
+                            required
+                        />
+                        <Field
+                            name="pass"
+                            label="Passord"
+                            component={renderPassField}
+                            required
+                        />
+                        <Field
+                            name="pass_rep"
+                            label="Gjenta Passord"
+                            component={renderPassField}
+                            required
+                        />
+                    </div>
+                    <div id="loginButtons">
+                        <RaisedButton className="form-button" primary={true} type="submit" label="Lagre" disabled={pristine || submitting}/>
+                        <RaisedButton className="form-button" secondary={true} label="Tilbake" onTouchTap={handleBackButton}/>
+                    </div>
+                </form>
+            </Paper>
         );
     }
 
