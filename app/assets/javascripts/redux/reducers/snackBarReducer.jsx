d93774de57b54b0a423201123503f187f20b09d5
Vestein Dahl <vesteind@stud.ntnu.no>
2017-05-09 11:34:56 +0200

    Adds stuff.

--- app/assets/javascripts/redux/reducers/snackBarReducer.jsx
+++ app/assets/javascripts/redux/reducers/snackBarReducer.jsx
@@ -3,8 +3,9 @@ import {
     POST_UPDATE_REQUIREMENT,
     POST_ADD_REQUIREMENT,
     INSERT_HAS_ACCESS,
-    REMOVE_HAS_ACCESS
-
+    REMOVE_HAS_ACCESS,
+    POST_REQUIREMENT_TO_PROJECT,
+    DELETE_REQUIREMENT_TO_PROJECT
 } from './../types';
 
 const snackBarReducer = (state = {
@@ -12,6 +13,26 @@ const snackBarReducer = (state = {
     text: ''
 }, action) => {
     switch (action.type) {
+        case DELETE_REQUIREMENT_TO_PROJECT.RECEIVED:
+            return {
+                isOpen: true,
+                text: "Krav slettet fra prosjekt!"
+            };
+        case DELETE_REQUIREMENT_TO_PROJECT.ERROR:
+            return {
+                isOpen: true,
+                text: "Ingen tilgang."
+            };
+        case POST_REQUIREMENT_TO_PROJECT.RECEIVED:
+            return {
+                isOpen: true,
+                text: "Krav lagt til!"
+            };
+        case POST_REQUIREMENT_TO_PROJECT.ERROR:
+            return {
+                isOpen: true,
+                text: "Ingen tilgang."
+            };
         case POST_UPDATE_REQUIREMENT.RECEIVED:
             return {
                 isOpen: true,
@@ -31,22 +52,22 @@ const snackBarReducer = (state = {
             return {
                 isOpen: true,
                 text: action.response.data
-            }
+            };
         case INSERT_HAS_ACCESS.ERROR:
             return {
                 isOpen: true,
                 text: action.response.data
-            }
+            };
         case REMOVE_HAS_ACCESS.RECEIVED:
             return {
                 isOpen: true,
                 text: action.response.data
-            }
+            };
         case REMOVE_HAS_ACCESS.ERROR:
             return {
                 isOpen: true,
                 text: action.response.data
-            }
+            };
         default:
             return state
     }
